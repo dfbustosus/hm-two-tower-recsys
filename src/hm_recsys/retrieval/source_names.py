e3629def73043fe88e64bd794ba7dfb96678abca
David BU <dfbustos@uninorte.edu.co>
2026-06-02 16:05:00 -0500

    Add retrieval source names

---
+++ src/hm_recsys/retrieval/source_names.py
@@ -0,0 +1,9 @@
+"""Canonical retrieval source names used across diagnostics and exports."""
+
+REPEAT_SOURCE = "repeat"
+RECENT_POPULARITY_SOURCE = "recent_popularity"
+ALL_TIME_POPULARITY_SOURCE = "all_time_popularity"
+REPEAT_POPULARITY_BLEND_SOURCE = "repeat_popularity_blend"
+CO_VISITATION_SOURCE = "co_visitation"
+REPEAT_CO_VISITATION_POPULARITY_BLEND_SOURCE = "repeat_co_visitation_popularity_blend"
+REPEAT_POPULARITY_CO_VISITATION_BLEND_SOURCE = "repeat_popularity_co_visitation_blend"
